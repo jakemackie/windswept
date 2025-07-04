3dd3145544eeeef19ddf4af046f484315ac3702b
Jake Mackie <jake@hiyield.co.uk>
2025-07-04 14:10:22 +0100

    feat: improved setup

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 BUILD_FLAG=""
-
 if [[ "$1" == "--build" ]]; then
    BUILD_FLAG="--build"
 fi
 
-if [[ -n "$BUILD_FLAG" ]]; then
-   echo "Building and starting container..."
-   doppler run -- docker-compose up $BUILD_FLAG
-else
-   echo "Starting container without building..."
-   doppler run -- docker-compose up
-fi
+# Run docker with doppler secrets
+echo -e "🐳 Docker container starting..."
+doppler run -- docker-compose up $BUILD_FLAG
