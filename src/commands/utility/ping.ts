9af113c4fe555f2fe9a95fb5db911a192523e8a2
Jake Mackie <jake@hiyield.co.uk>
2025-07-07 19:30:32 +0100

    feat: example prefix command handler and improved local dev

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -1,5 +1,8 @@
 import { SlashCommandBuilder } from 'discord.js';
-import type { CommandInteraction } from 'discord.js';
+import type { 
+	CommandInteraction,
+	Message
+} from 'discord.js';
 
 export default {
 	name: 'ping',
@@ -17,5 +20,17 @@ export default {
 		const apiLatency = postInteractionReply - preInteractionReply;
 
 		await interaction.editReply(`bot latency: \`${clientLatency}ms\` api latency: \`${apiLatency}ms\``);
+	},
+
+	async prefix(message: Message, _args: string[]) {
+		const clientLatency = Math.round(message.client.ws.ping);
+		const preMessageReply = Date.now();
+
+		let initialMessageReply = await message.reply(`bot latency: \`${clientLatency}ms\``);
+
+		const postMessageReply = Date.now();
+		const apiLatency = postMessageReply - preMessageReply;
+
+		await initialMessageReply.edit(`bot latency: \`${clientLatency}ms\` api latency: \`${apiLatency}ms\``);
 	}
 };
