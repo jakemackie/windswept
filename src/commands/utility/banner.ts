cb124b917142bc85000bf365d83c07a84ac4083b
Jake Mackie <jake@hiyield.co.uk>
2025-07-10 14:33:07 +0100

    amend: indentation

--- src/commands/utility/banner.ts
+++ src/commands/utility/banner.ts
@@ -4,45 +4,45 @@ import {
     MediaGalleryBuilder,
     MediaGalleryItemBuilder,
     MessageFlags
-  } from 'discord.js';
-  import type { ChatInputCommandInteraction } from 'discord.js';
+} from 'discord.js';
+import type { ChatInputCommandInteraction } from 'discord.js';
   
-  export default {
-    name: 'banner',
-    data: new SlashCommandBuilder()
-    .setName('banner')
-    .setDescription('Fetch the banner of the specified user, defaults to self')
-    .addUserOption(option => option
-      .setName('user')
-      .setDescription('The user to fetch the banner of')
-      .setRequired(false)
-    ),
-  
-    async execute(interaction: ChatInputCommandInteraction) {
-      const user = interaction.options.getUser('user') || interaction.user;
-      // Fetch the full user object to ensure banner is available
-      const fullUser = await interaction.client.users.fetch(user.id, { force: true });
-      const bannerUrl = fullUser.bannerURL({ size: 1024 });
+export default {
+  name: 'banner',
+  data: new SlashCommandBuilder()
+  .setName('banner')
+  .setDescription('Fetch the banner of the specified user, defaults to self')
+  .addUserOption(option => option
+    .setName('user')
+    .setDescription('The user to fetch the banner of')
+    .setRequired(false)
+  ),
 
-      if (!bannerUrl) {
-        return await interaction.reply({
-          content: `${user.username} does not have a banner.`,
-          flags: MessageFlags.Ephemeral
-        });
-      }
+  async execute(interaction: ChatInputCommandInteraction) {
+    const user = interaction.options.getUser('user') || interaction.user;
+    // Fetch the full user object to ensure banner is available
+    const fullUser = await interaction.client.users.fetch(user.id, { force: true });
+    const bannerUrl = fullUser.bannerURL({ size: 1024 });
 
-      const container = new ContainerBuilder();
-      const mediaGallery = new MediaGalleryBuilder()
-        .addItems(new MediaGalleryItemBuilder()
-          .setURL(bannerUrl)
-          .setDescription(`The banner of ${user.username}`)
-        );
-
-      container.addMediaGalleryComponents(mediaGallery);
-
-      await interaction.reply({
-        components: [container],
-        flags: MessageFlags.IsComponentsV2
+    if (!bannerUrl) {
+      return await interaction.reply({
+        content: `${user.username} does not have a banner.`,
+        flags: MessageFlags.Ephemeral
       });
     }
+
+    const container = new ContainerBuilder();
+    const mediaGallery = new MediaGalleryBuilder()
+      .addItems(new MediaGalleryItemBuilder()
+        .setURL(bannerUrl)
+        .setDescription(`The banner of ${user.username}`)
+      );
+
+    container.addMediaGalleryComponents(mediaGallery);
+
+    await interaction.reply({
+      components: [container],
+      flags: MessageFlags.IsComponentsV2
+    });
+  }
 };
