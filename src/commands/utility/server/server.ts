f8ad19180b7259fe32fbc9b7b213bd6de8ce7582
jakemackie <jake.wmackie@gmail.com>
2025-08-03 17:17:41 +0100

    feat: server splash

--- src/commands/utility/server/server.ts
+++ src/commands/utility/server/server.ts
@@ -5,6 +5,7 @@ import {
 } from 'discord.js';
 import avatar from './serverAvatar.js';
 import firstmessage from './serverFirstMessage.js';
+import serverSplash from './serverSplash.js';
 
 export default {
   data: new SlashCommandBuilder()
@@ -17,6 +18,10 @@ export default {
     .addSubcommand(sub =>
       sub.setName(firstmessage.data.name)
         .setDescription(firstmessage.data.description)
+    )
+    .addSubcommand(sub =>
+      sub.setName(serverSplash.data.name)
+        .setDescription(serverSplash.data.description)
     ),
 
   async execute(interaction: ChatInputCommandInteraction) {
@@ -27,6 +32,8 @@ export default {
         return avatar.execute(interaction);
       case 'firstmessage':
         return firstmessage.execute(interaction);
+      case 'splash':
+        return serverSplash.execute(interaction);
     }
 
     return interaction.reply({ 
