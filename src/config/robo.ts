84c7447ebbd746c3788264b4cdc6ac6683e70edc
Jake Mackie <jake@hiyield.co.uk>
2025-08-07 22:02:05 +0100

    amend: remove files and start from base

--- src/config/robo.ts
+++
@@ -1,7 +0,0 @@
-import { GatewayIntentBits } from 'discord.js'
-
-export default {
-	clientOptions: {
-		intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]
-	}
-}
--- src/events/_start.ts
+++
@@ -1,8 +0,0 @@
-import { client } from 'robo.js'
-import type { Message } from 'discord.js'
-
-export default () => {
-	client.on('messageCreate', (message: Message) => {
-		console.log(message.content)
-	})
-}
