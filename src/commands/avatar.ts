186a79812e5c0999deae5d8b60984a334f1d9cc2
Jake Mackie <jake@hiyield.co.uk>
2025-08-08 18:38:38 +0100

    amend: formatting consistency changes, prisma client logs

--- src/commands/avatar.ts
+++ src/commands/avatar.ts
@@ -9,48 +9,49 @@ import { createCommandConfig, type CommandOptions, type CommandResult } from 'ro
 export const config = createCommandConfig(
 	{
 		description: 'Fetch the avatar of the specified user, defaults to self',
-        options: [
+      options: [
+        {
+          name: 'user',
+          type: 'user',
+          description: 'The user to fetch the avatar of',
+          required: false
+        },
+        {
+          name: 'size',
+          type: 'integer',
+          description: 'The size of the avatar to fetch',
+          choices: [
             {
-                name: 'user',
-                type: 'user',
-                description: 'The user to fetch the avatar of',
-                required: false
+              name: 'Small',
+              value: 1024
             },
             {
-                name: 'size',
-                type: 'integer',
-                description: 'The size of the avatar to fetch',
-                choices: [
-                    {
-                        name: 'Small',
-                        value: 1024
-                    },
-                    {
-                        name: 'Medium',
-                        value: 2048
-                    },
-                    {
-                        name: 'Large',
-                        value: 4096
-                    }
-                ],
-                required: false
+              name: 'Medium',
+              value: 2048
+            },
+            {
+              name: 'Large',
+              value: 4096
             }
-        ]
+          ],
+          
+          required: false
+        }
+      ]
 	} as const
 );
 
 export default async (interaction: ChatInputCommandInteraction, options: CommandOptions<typeof config>): Promise<CommandResult> => {
-    const user = options.user || interaction.user;
-    const size = options.size || 1024;
+  const user = options.user || interaction.user;
+  const size = options.size || 1024;
 
-    const container = new ContainerBuilder()
-      .addMediaGalleryComponents((builder) => builder.addItems(
-        (builder) => builder.setURL(user.displayAvatarURL({ size: size }))
-      ))
+  const container = new ContainerBuilder()
+    .addMediaGalleryComponents((builder) => builder.addItems(
+      (builder) => builder.setURL(user.displayAvatarURL({ size }))
+    ))
 
-    await interaction.reply({
-      components: [container],
-      flags: MessageFlags.IsComponentsV2
-    })
+  await interaction.reply({
+    components: [container],
+    flags: MessageFlags.IsComponentsV2
+  })
 }
